3928dc634fba12871255762e2c7db95f0db1071c
Kevin Nava <1723110440@utectulancingo.edu.mx>
2024-05-24 14:55:57 +0000

    UPDATE README

---
+++ aplicacion/app.py
@@ -0,0 +1,10 @@
+import requests
+
+url = 'http://127.0.0.1:11434/api/generate'
+myobj = {
+  "model": "tinyllama",
+  "prompt":"¿porqué el cielo es azul?",
+  "stream": False
+}
+x = requests.post(url, json = myobj)
+print(x.text)
